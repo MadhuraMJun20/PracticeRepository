61537b70f6506ea0cadd8f49e7239ebcd478f24c
Madhura Mulay <madhurah02@gmail.com>
2020-08-06 23:28:41 +0530

    Adding one syso statement

--- src/package1/Practice_3.java
+++ src/package1/Practice_3.java
@@ -3,5 +3,6 @@ package package1;
 public class Practice_3 {
 	public static void main(String[] args) {
 		System.out.println();
+		System.out.println("Rename branch and delete remote branch");
 	}
 }
